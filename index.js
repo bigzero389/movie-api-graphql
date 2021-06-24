78c41363f12223f0ce497592b4a8d4a5e60009ae
bigzero <architect@HIST-Architectures-MacBook-Air.local>
2021-06-24 14:09:37 +0900

    port change from 4000 to 80

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 
+const PORT = 80;
 const server = new GraphQLServer({
   typeDefs: "graphql/schema.graphql",
   resolvers: resolvers
 }); 
 
-server.start(() => console.log("Graphql Server Running"));
+server.start({port : PORT},() => console.log("Graphql Server Running"));
 //console.log("bigzero Hello");
