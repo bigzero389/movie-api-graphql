1ad8bdcf45992489459e7179bd1800f2f2d9fdd8
bigzero <garack@gmail.com>
2024-04-30 13:57:46 +0900

    fix : graphql-yoga library version-up

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-import { GraphQLServer } from "graphql-yoga";
-import resolvers from "./graphql/resolvers";
+import { createServer } from 'node:http';
+import { createYoga } from "graphql-yoga";
+// import resolvers from './graphql/resolvers';
+// 왜 아래처럼 하드코딩 하면 되고 resolvers 를 import 해서 쓰려고 하면 안되는지 이해가 안됨.
 
-const server = new GraphQLServer({
+const yoga = createYoga({
   typeDefs: "graphql/schema.graphql",
-  resolvers: resolvers
+  resolvers: {
+    Query: {
+      movies: (_, {limit, rating}) => getMovies(limit, rating),
+      movie: (_, { id }) => getMovie(id),
+      suggestions: (_, { id }) => getSuggestions(id)
+    }
+    /*
+    ,
+      Mutation: {
+      addMovie: (_, {name, score}) => addMovie(name, score),
+      deleteMovie: (_, {id}) => deleteMovie(id)
+    }
+    */
+  }
 }); 
 
-server.start(() => console.log("Graphql Server Running"));
-//console.log("bigzero Hello");
-
-
+const server = createServer(yoga);
+server.listen(4000, () => {
+  console.log("bigzero Hello");
+})
